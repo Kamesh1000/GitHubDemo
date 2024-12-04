9b9dbbaf2b7212f20c302f23eca66f80c343db35
Kamesh1000 <kameshbehera1000@gmail.com>
2024-12-04 16:09:03 +0530

    Project Created

---
+++ src/test/java/com/example/ghd/GitHubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
